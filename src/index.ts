5f1d2fb74261a200b966e8d41e3b1c838cbf821e
cole <bernankez@qq.com>
2023-05-07 02:54:20 +0800

    fix: top-level await

--- src/index.ts
+++ src/index.ts
@@ -5,38 +5,45 @@ import { connect } from "./database/connect";
 import { Counter, Log } from "./database/schema";
 import { increment } from "./database/service";
 
-const app = createApp();
+async function initWebServer() {
+  const app = createApp();
 
-const mongodb = await connect();
+  const mongodb = await connect();
 
-mongodb.connection.on("disconnected", () => {
-  consola.info("mongodb disconnected");
-});
+  mongodb.connection.on("disconnected", () => {
+    consola.info("mongodb disconnected");
+  });
 
-consola.success("mongodb connected");
+  consola.success("mongodb connected");
 
-await Log.ensureIndexes();
-await Counter.ensureIndexes();
+  await Log.ensureIndexes();
+  await Counter.ensureIndexes();
 
-consola.success("mongodb indexes ensured");
+  consola.success("mongodb indexes ensured");
 
-const router = createRouter().get("/api/**", eventHandler(async (event) => {
-  const headers = getHeaders(event);
-  const [namespace, key] = event.context.params?._?.split("/") ?? [];
-  if (!namespace) {
-    throw createError({
-      statusCode: 400,
-      statusMessage: "namespace is required",
-    });
-  } else if (!key) {
-    throw createError({
-      statusCode: 400,
-      statusMessage: "key is required",
-    });
-  }
-  return await increment(namespace, key, headers);
-}));
+  const router = createRouter().get("/api/**", eventHandler(async (event) => {
+    const headers = getHeaders(event);
+    const [namespace, key] = event.context.params?._?.split("/") ?? [];
+    if (!namespace) {
+      throw createError({
+        statusCode: 400,
+        statusMessage: "namespace is required",
+      });
+    } else if (!key) {
+      throw createError({
+        statusCode: 400,
+        statusMessage: "key is required",
+      });
+    }
+    // TODO check referer to decide whether to increment
+    // if not, return 401
+    // TODO node-cache for 10 seconds
+    return await increment(namespace, key, headers);
+  }));
 
-app.use(router);
+  app.use(router);
 
-createServer(toNodeListener(app)).listen(process.env.PORT || 3000);
+  createServer(toNodeListener(app)).listen(process.env.PORT || 3000);
+}
+
+initWebServer();

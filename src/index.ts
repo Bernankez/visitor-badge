b778dfad1b00ccfbdd91462c0edc88631b63f73c
cole <bernankez@qq.com>
2023-05-06 17:33:48 +0800

    feat: add h3

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-export const one = 1;
-export const two = 2;
+import { createServer } from "node:http";
+import { createApp, createRouter, eventHandler, toNodeListener } from "h3";
+
+const app = createApp();
+
+const router = createRouter().get("/", eventHandler(() => "Hello world!"));
+
+app.use(router);
+
+createServer(toNodeListener(app)).listen(process.env.PORT || 3000);

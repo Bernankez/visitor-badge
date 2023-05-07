749f5f4917a0f313e67216ef079228dd6b2ea694
cole <bernankez@qq.com>
2023-05-08 03:51:43 +0800

    chore: fix tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,17 +3,11 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   clean: true,
-  format: ["cjs", "esm"],
+  format: ["cjs"],
   shims: true,
-  dts: true,
   minify: true,
   treeshake: true,
   loader: {
     ".svg": "dataurl",
-  },
-  outExtension({ format }) {
-    return {
-      js: `.${format === "esm" ? "mjs" : format}`,
-    };
   },
 });

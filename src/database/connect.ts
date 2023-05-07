2001f03b1fe6fc9a19c54f0ee81acfec0d12a895
cole <bernankez@qq.com>
2023-05-08 03:00:12 +0800

    fix: mongoose connection

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -5,15 +5,16 @@ import { loadConfig } from "../utils/load-config";
 let globalMongoose: Mongoose | undefined;
 
 export async function connect() {
-  const { mongodbDatabase, mongodbHost, mongodbPassword, mongodbPort, mongodbUser } = await loadConfig();
-  let url = "";
-  if (mongodbUser && mongodbPassword) {
-    url = `mongodb://${mongodbUser}:${mongodbPassword}@${mongodbHost}:${mongodbPort}`;
-  } else {
-    url = `mongodb://${mongodbHost}:${mongodbPort}`;
-  }
+  const { mongodbDatabase, mongodbHost, mongodbPassword, mongodbPort, mongodbUser, mongodbSRV } = await loadConfig();
+  const url = `mongodb${mongodbSRV ? "+srv" : ""}://${mongodbHost}${mongodbPort ? `:${mongodbPort}` : ""}`;
   return mongoose.connect(url, {
+    user: mongodbUser,
+    pass: mongodbPassword,
     dbName: mongodbDatabase,
+    retryWrites: true,
+    writeConcern: {
+      w: "majority",
+    },
   }).then((res) => {
     globalMongoose = res;
     return res;
--- src/utils/load-config.ts
+++ src/utils/load-config.ts
@@ -1,13 +1,16 @@
 import { loadConfig as _loadConfig } from "c12";
 
 export async function loadConfig() {
-  await _loadConfig({});
+  await _loadConfig({
+    dotenv: true,
+  });
   const {
     MONGODB_HOST: mongodbHost = "localhost",
     MONGODB_PORT: mongodbPort = 27017,
     MONGODB_DATABASE: mongodbDatabase = "counter",
     MONGODB_USER: mongodbUser,
     MONGODB_PASSWORD: mongodbPassword,
+    MONGODB_SRV: mongodbSRV = "false",
   } = process.env;
   return {
     mongodbDatabase,
@@ -15,5 +18,6 @@ export async function loadConfig() {
     mongodbPassword,
     mongodbPort,
     mongodbUser,
+    mongodbSRV: mongodbSRV === "true",
   };
 }

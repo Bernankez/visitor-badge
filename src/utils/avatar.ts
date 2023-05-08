3ca165a3a65b0c844dd078f3697cfe3ce62d3fe1
cole <bernankez@qq.com>
2023-05-08 11:30:03 +0800

    fix(avatar): datauri escape

--- src/utils/avatar.ts
+++ src/utils/avatar.ts
@@ -2,7 +2,7 @@ import dataUri from "../assets/avatar.svg";
 
 export function resolveAvatar(x = 0, y = 0, width = 64, height = 64) {
   const image = `
-    <image x="${x}" y="${y}" width="${width}" height="${height}" xlink:href="${dataUri}"></image>
+    <image x="${x}" y="${y}" width="${width}" height="${height}" xlink:href="${dataUri.replaceAll("\"\"", "\\\"\\\"")}"></image>
   `;
 
   return image;

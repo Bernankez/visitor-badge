662cd08650e8ca58e1be3634e8d91f89694691a8
cole <bernankez@qq.com>
2023-05-08 10:28:59 +0800

    chore: move out index

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
 import { createServer } from "node:http";
 import { createApp, toNodeListener } from "h3";
 import consola from "consola";
-import { connect } from "./database/connect";
-import { Counter, Log } from "./database/schema";
-import { createRouter } from "./api";
+import { connect } from "../src/database/connect";
+import { Counter, Log } from "../src/database/schema";
+import { createRouter } from "../src/api";
 
 async function initWebServer() {
   const app = createApp();
